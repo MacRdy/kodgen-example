982670f8a66a5faddccf514ef37c22e255bbd61b
Anton Shibaev <macready.dev@gmail.com>
2023-07-24 22:05:58 +0300

    configuration

---
+++ kodgen-typescript/ng-typescript/petstore3/hooks.js
@@ -0,0 +1,6 @@
+exports.module = {
+    // 'I' prefix
+    generateModelName: (defaultFn, name, modifier, type) => `I${defaultFn(name, modifier, type)}`,
+    // Add 'Api' suffix to service name
+    generateServiceName: (defaultFn, name, modifier) => `${defaultFn(name, modifier)}Api`,
+};
